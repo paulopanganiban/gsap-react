bbc706f660a14c7cc6de8b1088ce5a42152b480d
John Paulo Panganiban <olopangs@gmail.com>
2022-02-16 05:54:14 +0800

    end of gsap studies

---
+++ src/components/Layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+const Layout = () => {
+  return (
+    <LayoutContainer>Layout</LayoutContainer>
+  )
+}
+
+export default Layout
+const LayoutContainer = styled.div``
